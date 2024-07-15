e12f1e84581e7e24aebd04fdd269c11560a6ec0b
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-07-15 10:05:27 -0300

    feat: add Roadmap component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
+import Rodmap from "./components/RodMap";
 import Services from "./components/Services";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Collaboration />
         <Services />
         <Pricing />
+        <Rodmap />
       </div>
       <ButtonGradient />
     </>
