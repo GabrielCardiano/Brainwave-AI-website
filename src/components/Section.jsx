489b6c257b59cb252741443f78277058ab651fd8
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-07-15 16:29:44 -0300

    fix: Roadmap name and  Section CSS styles

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -15,12 +15,9 @@ function Section({
         relative
          ${
            customPaddings ||
-           `py-10 
-           lg:py-16 
-           xl:py-20 
-           ${crosses ? "lg:py-32 xl:py-40" : ""} 
-           ${className || ""}`
-         }`}
+           `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
+         }
+           ${className || ""}`}
     >
       {children}
 
@@ -28,40 +25,31 @@ function Section({
         className="
           hidden
           absolute
-          top-0
-          left-5
-          w-0.25
-          h-full
+          top-0 left-5 w-0.25 h-full
           bg-stroke-1
           pointer-events-none
           md:block
-          lg:left-7.5
-          xl:left-7.5
-          lg:right-7.5
-          xl:right-7.5"
-      >
-        {crosses && (
-          <>
-            <div
-              className={`
+          lg:left-7.5 xl:left-10
+          lg:right-7.5 xl:right-10"
+      />
+
+      {crosses && (
+        <>
+          <div
+            className={`
                 hidden 
                 absolute 
-                top-0 
-                left-7.5 
-                right-7.5 
-                h-0.25 
+                top-0 left-7.5 right-7.5 h-0.25
                 bg-stroke-1
                 ${crossesOffset && crossesOffset}
                 pointer-events-none 
                 lg:block
                 xl:left-10
                 right-10`}
-            >
-              <SectionSvg crossesOffset={crossesOffset} />
-            </div>
-          </>
-        )}
-      </div>
+          />
+          <SectionSvg crossesOffset={crossesOffset} />
+        </>
+      )}
     </div>
   );
 }
